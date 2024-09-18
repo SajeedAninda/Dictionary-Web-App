454d65a99a899119e0f773baceb99da32b6cc8c9
SajeedAninda <sajaninda@gmail.com>
2024-09-18 16:36:50 +0600

    Created the searchbar

--- src/Components/Dictionary.jsx
+++ src/Components/Dictionary.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import searchIcon from "../assets/icon-search.svg"
 
 
 const Dictionary = () => {
     return (
-        <div>
-
+        <div className='w-[55%] mx-auto'>
+            <div className='searchBar relative'>
+                <input className="w-full rounded-2xl py-5 bg-[#f4f4f4] px-6" placeholder='Search for a word from the dictionary'/>
+                <img className='absolute right-5 top-6' src={searchIcon} alt="" />
+            </div>
         </div>
     );
 };
