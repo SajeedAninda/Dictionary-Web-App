fe2c78bc53028a1e963184c5d3d450036cfb3cb0
SajeedAninda <sajaninda@gmail.com>
2024-09-20 17:14:43 +0600

    Mapped data from api into dummy texts

--- src/Components/Dictionary.jsx
+++ src/Components/Dictionary.jsx
@@ -4,15 +4,17 @@ import playLogo from "../assets/icon-play.svg"
 
 
 const Dictionary = () => {
-    let [searchedWord, setSearchedWord] = useState("");
+    let [searchedWord, setSearchedWord] = useState([]);
 
     useEffect(() => {
-        fetch('https://api.dictionaryapi.dev/api/v2/entries/en/hello')
+        fetch('https://api.dictionaryapi.dev/api/v2/entries/en/love')
             .then(response => response.json())
             .then(json => setSearchedWord(json))
     }, [])
 
-    console.log(searchedWord);
+    let [finalWord] = searchedWord;
+
+    console.log(finalWord);
 
     return (
         <div className='w-[55%] mx-auto'>
@@ -26,49 +28,68 @@ const Dictionary = () => {
                     <div className='flex justify-between items-center pt-16 pb-8'>
                         <div>
                             <h1 className='text-[46px] text-[#2d2d2d] font-bold'>
-                                Keyboard
+                                {finalWord?.word}
                             </h1>
 
-                            <p className='text-[24px] text-[#a445ed] font-semibold'>
-                                /ˈkiːbɔːd/
-                            </p>
+                            {
+                                finalWord?.phonetic &&
+                                <p className='text-[24px] text-[#a445ed] font-semibold'>
+                                    {
+                                        finalWord?.phonetic
+                                    }
+                                </p>
+                            }
                         </div>
 
                         <div>
                             <img className='w-[56px] cursor-pointer hover:opacity-55' src={playLogo} alt="" />
                         </div>
                     </div>
+                </div>
 
-                    <div className='flex justify-between items-center gap-4'>
-                        <p className='text-[24px] font-semibold text-[#2d2d2d] w-[10%]'>
-                            noun
-                        </p>
+                {
+                    finalWord?.meanings?.map(word =>
+                        <div className="lowerText mt-8">
+                            <div className='flex justify-between items-center gap-4 mt-2'>
+                                <p className='text-[24px] font-semibold text-[#2d2d2d] w-[20%]'>
+                                    {word?.partOfSpeech}
+                                </p>
 
-                        <div className='w-[90%] h-[1px] bg-[#2d2d2d]'>
+                                <div className='w-[80%] h-[1px] bg-[#2d2d2d]'>
+                                </div>
+                            </div>
+
+                            <p className='text-[16px] text-[#757575] mt-8'>
+                                Meaning
+                            </p>
+                            <ul className='pl-12 mt-4'>
+                                {
+                                    word?.definitions?.map(defs =>
+                                        <li className='list-disc text-[16px] text-[#2d2d2d] marker:text-[#a445ed] mt-2'>
+                                            {defs?.definition}
+                                        </li>
+                                    )
+                                }
+                            </ul>
+
+                            <div className='flex gap-2 mt-8'>
+                                <p className='text-[16px] text-[#2d2d2d]'>
+                                    Synonyms:
+                                </p>
+
+                                <div className='flex flex-wrap gap-2'>
+                                    {
+                                        word?.synonyms?.map((synms, index) => (
+                                            <p key={index} className='text-[16px] text-[#a445ed] font-bold hover:underline cursor-pointer'>
+                                                {synms},
+                                            </p>
+                                        ))
+                                    }
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-
-                <div className="lowerText mt-8">
-                    <p className='text-[16px] text-[#757575]'>
-                        Meaning
-                    </p>
-                    <ul className='pl-12 mt-4'>
-                        <li className='list-disc text-[16px] text-[#2d2d2d] marker:text-[#a445ed]'>
-                            A component of many instruments including the piano, organ, and harpsichord consisting of usually black and white keys that cause different tones to be produced when struck.
-                        </li>
-                    </ul>
-
-                    <div className='flex gap-2 items-center mt-8'>
-                        <p className='text-[16px] text-[#2d2d2d]'>
-                            Synonyms:
-                        </p>
-
-                        <p className='text-[16px] text-[#a445ed] font-bold hover:underline cursor-pointer'>
-                            Electronic Keyboard
-                        </p>
-                    </div>
-                </div>
+                    )
+                }
             </div>
         </div>
     );
