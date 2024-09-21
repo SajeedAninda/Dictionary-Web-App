51a71bca745260fb4837d42ff4280320ecee839c
SajeedAninda <sajaninda@gmail.com>
2024-09-21 16:51:48 +0600

    Toggled theme switcher button with transition

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import logo from "../assets/logo.svg"
-import downArrow from "../assets/icon-arrow-down.svg"
+import React, { useState } from 'react';
+import logo from "../assets/logo.svg";
+import downArrow from "../assets/icon-arrow-down.svg";
 import moonLogo from "../assets/icon-moon.svg";
 
 const Navbar = () => {
+    const [isToggled, setIsToggled] = useState(false);
+
+    const handleToggle = () => {
+        setIsToggled(!isToggled);
+    };
+
     return (
         <div className='w-[55%] mx-auto'>
             <div className='navbar h-[20vh] flex justify-between items-center'>
@@ -19,10 +25,13 @@ const Navbar = () => {
                         <img src={downArrow} alt="" />
                     </div>
 
-                    <button className='w-[40px] h-[20px] rounded-2xl bg-[#979797] flex items-center px-[0.15rem] cursor-pointer hover:bg-[#A445ed] transition-colors delay-50'>
-                        <div className='w-[14px] h-[14px] bg-[#f4f4f4] rounded-full'>
-
-                        </div>
+                    <button 
+                        className='themeSwitcher w-[40px] h-[20px] rounded-2xl bg-[#979797] flex items-center px-[0.15rem] cursor-pointer hover:bg-[#A445ed] transition-colors delay-50'
+                        onClick={handleToggle}
+                    >
+                        <div 
+                            className={`w-[14px] h-[14px] bg-[#f4f4f4] rounded-full transition-transform duration-300 ${isToggled ? 'translate-x-[1.2rem]' : 'translate-x-0'}`}
+                        ></div>
                     </button>
 
                     <div>
