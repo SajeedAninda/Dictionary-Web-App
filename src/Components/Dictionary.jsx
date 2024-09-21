1b0e75d8d13f415e6667b9590e0bcacef1fe631e
SajeedAninda <sajaninda@gmail.com>
2024-09-21 17:27:23 +0600

    Added dark mode all across the website

--- src/Components/Dictionary.jsx
+++ src/Components/Dictionary.jsx
@@ -37,102 +37,104 @@ const Dictionary = () => {
     };
 
     return (
-        <div className='w-[55%] mx-auto'>
-            <div className='searchBar relative'>
-                <input
-                    className="w-full rounded-2xl py-5 bg-[#f4f4f4] px-6"
-                    placeholder='Search for a word from the dictionary'
-                    onKeyPress={handleSearch}
-                />
-                <img className='absolute right-5 top-6' src={searchIcon} alt="Search Icon" />
-            </div>
-
-            <div className='textSection'>
-                <div className="upperText">
-                    <div className='flex justify-between items-center pt-16 pb-8'>
-                        <div>
-                            <h1 className='text-[46px] text-[#2d2d2d] font-bold'>
-                                {finalWord?.word}
-                            </h1>
-
-                            {finalWord?.phonetic && (
-                                <p className='text-[24px] text-[#a445ed] font-semibold'>
-                                    {finalWord?.phonetic}
-                                </p>
-                            )}
-                        </div>
-
-                        <div>
-                            {audioUrl && (
-                                <img
-                                    className='w-[56px] cursor-pointer hover:opacity-55'
-                                    src={playLogo}
-                                    alt="Play Icon"
-                                    onClick={playAudio}
-                                />
-                            )}
-                        </div>
-                    </div>
+        <div className='dark:bg-[#151515]'>
+            <div className='w-[55%] mx-auto'>
+                <div className='searchBar relative'>
+                    <input
+                        className="w-full rounded-2xl py-5 bg-[#f4f4f4] dark:bg-[#2e2e2e] px-6"
+                        placeholder='Search for a word from the dictionary'
+                        onKeyPress={handleSearch}
+                    />
+                    <img className='absolute right-5 top-6' src={searchIcon} alt="Search Icon" />
                 </div>
 
-                {finalWord?.meanings?.map((word, index) => (
-                    <div key={index} className="lowerText mt-8">
-                        <div className='flex justify-between items-center gap-4 mt-2'>
-                            <p className='text-[30px] font-semibold text-[#2d2d2d] w-[20%]'>
-                                {word?.partOfSpeech}
-                            </p>
+                <div className='textSection'>
+                    <div className="upperText">
+                        <div className='flex justify-between items-center pt-16 pb-8'>
+                            <div>
+                                <h1 className='text-[46px]  dark:text-[#f2f2f2] font-bold'>
+                                    {finalWord?.word}
+                                </h1>
 
-                            <div className='w-[80%] h-[1px] bg-[#2d2d2d]'></div>
+                                {finalWord?.phonetic && (
+                                    <p className='text-[24px] text-[#a445ed] font-semibold'>
+                                        {finalWord?.phonetic}
+                                    </p>
+                                )}
+                            </div>
+
+                            <div>
+                                {audioUrl && (
+                                    <img
+                                        className='w-[56px] cursor-pointer hover:opacity-55'
+                                        src={playLogo}
+                                        alt="Play Icon"
+                                        onClick={playAudio}
+                                    />
+                                )}
+                            </div>
                         </div>
-
-                        <p className='text-[16px] text-[#757575] mt-8'>
-                            Meaning
-                        </p>
-                        <ul className='pl-12 mt-4'>
-                            {word?.definitions?.map((defs, idx) => (
-                                <li key={idx} className='list-disc text-[16px] text-[#2d2d2d] marker:text-[#a445ed] mt-2'>
-                                    {defs?.definition}
-                                </li>
-                            ))}
-                        </ul>
-
-                        {word?.synonyms && word.synonyms.length > 0 && (
-                            <div className='flex gap-2 mt-8'>
-                                <p className='text-[16px] text-[#2d2d2d]'>
-                                    Synonyms:
-                                </p>
-
-                                <div className='flex flex-wrap gap-2'>
-                                    {word?.synonyms?.map((synms, idx) => (
-                                        <p key={idx} className='text-[16px] text-[#a445ed] font-bold hover:underline cursor-pointer'>
-                                            {synms},
-                                        </p>
-                                    ))}
-                                </div>
-                            </div>
-                        )}
-
-                        {word?.antonyms && word.antonyms.length > 0 && (
-                            <div className='flex gap-2 mt-4'>
-                                <p className='text-[16px] text-[#2d2d2d]'>
-                                    Antonyms:
-                                </p>
-
-                                <div className='flex flex-wrap gap-2'>
-                                    {word?.antonyms?.map((antms, idx) => (
-                                        <p key={idx} className='text-[16px] text-[#a445ed] font-bold hover:underline cursor-pointer'>
-                                            {antms},
-                                        </p>
-                                    ))}
-                                </div>
-                            </div>
-                        )}
                     </div>
-                ))}
 
-                <p className='text-[16px] text-[#757575] mt-16 italic pb-12'>
-                    <span className='mr-6 underline'>Source:</span> <a className='underline' href={`https://api.dictionaryapi.dev/api/v2/entries/en/${searchInput}`} target='_blank' rel="noreferrer">https://api.dictionaryapi.dev/api/v2/entries/en/{searchInput}</a>
-                </p>
+                    {finalWord?.meanings?.map((word, index) => (
+                        <div key={index} className="lowerText mt-8">
+                            <div className='flex justify-between items-center gap-4 mt-2'>
+                                <p className='text-[30px] font-semibold text-[#2d2d2d] dark:text-[#f2f2f2] w-[20%]'>
+                                    {word?.partOfSpeech}
+                                </p>
+
+                                <div className='w-[80%] h-[1px] bg-[#2d2d2d]'></div>
+                            </div>
+
+                            <p className='text-[16px] text-[#757575] mt-8'>
+                                Meaning
+                            </p>
+                            <ul className='pl-12 mt-4'>
+                                {word?.definitions?.map((defs, idx) => (
+                                    <li key={idx} className='list-disc text-[16px] text-[#2d2d2d] dark:text-[#f2f2f2] marker:text-[#a445ed] mt-2'>
+                                        {defs?.definition}
+                                    </li>
+                                ))}
+                            </ul>
+
+                            {word?.synonyms && word.synonyms.length > 0 && (
+                                <div className='flex gap-2 mt-8'>
+                                    <p className='text-[16px] text-[#2d2d2d] dark:text-[#f2f2f2]'>
+                                        Synonyms:
+                                    </p>
+
+                                    <div className='flex flex-wrap gap-2'>
+                                        {word?.synonyms?.map((synms, idx) => (
+                                            <p key={idx} className='text-[16px] text-[#a445ed] font-bold hover:underline cursor-pointer'>
+                                                {synms},
+                                            </p>
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
+
+                            {word?.antonyms && word.antonyms.length > 0 && (
+                                <div className='flex gap-2 mt-4'>
+                                    <p className='text-[16px] text-[#2d2d2d] dark:text-[#f2f2f2]'>
+                                        Antonyms:
+                                    </p>
+
+                                    <div className='flex flex-wrap gap-2'>
+                                        {word?.antonyms?.map((antms, idx) => (
+                                            <p key={idx} className='text-[16px] text-[#a445ed] font-bold hover:underline cursor-pointer'>
+                                                {antms},
+                                            </p>
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                    ))}
+
+                    <p className='text-[16px] text-[#757575] mt-16 italic pb-12'>
+                        <span className='mr-6 underline'>Source:</span> <a className='underline' href={`https://api.dictionaryapi.dev/api/v2/entries/en/${searchInput}`} target='_blank' rel="noreferrer">https://api.dictionaryapi.dev/api/v2/entries/en/{searchInput}</a>
+                    </p>
+                </div>
             </div>
         </div>
     );
